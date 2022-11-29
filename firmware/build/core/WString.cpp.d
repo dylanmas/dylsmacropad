262f09117ef0acb8f979a5ca4ddc5f1783c16135
dylanmas <dylanmas687@gmail.com>
2022-11-29 08:39:08 -0600

    Initial placeholder

--- firmware/build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/dylanmasulis/Documents/macropad/build/core/WString.cpp.o: \
- /var/lib/flatpak/app/cc.arduino.arduinoide/x86_64/stable/33cefb673084f35323c64a09dc63f6766aee1a8c6268f95515ee89b2c5eb51c8/files/Arduino/hardware/arduino/avr/cores/arduino/WString.cpp \
- /var/lib/flatpak/app/cc.arduino.arduinoide/x86_64/stable/33cefb673084f35323c64a09dc63f6766aee1a8c6268f95515ee89b2c5eb51c8/files/Arduino/hardware/arduino/avr/cores/arduino/WString.h
